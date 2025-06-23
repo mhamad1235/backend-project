7472a71192651079e47a27d1ead29d2a5d422080
mhamad1235 <mhamadsalliim@gmail.com>
2025-06-23 17:38:36 +0300

    check credentials

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,9 +8,9 @@ window.Pusher = Pusher;
 // Initialize Echo
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT || 6001,
+    key: "my-app-key",
+    wsHost: '13.60.50.148',
+    wsPort: 6001,
     wssPort: 443,
     forceTLS: false,
     enabledTransports: ['ws', 'wss'],
